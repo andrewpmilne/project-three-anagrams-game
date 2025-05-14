21ede795bed38daa47e946cef970e2a7e46e3e3c
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-05-14 15:06:03 +0100

    link google sheet leaderboard with run.py

--- run.py
+++ run.py
@@ -4,6 +4,23 @@ import time
 import gspread
 from google.oauth2.service_account import Credentials
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('project_three_leaderboard')
+
+easy = SHEET.worksheet('easy')
+medium = SHEET.worksheet('medium')
+hard = SHEET.worksheet('hard')
+
+data = easy.get_all_values()
+print(data)
 
 def welcome_message():
     """
